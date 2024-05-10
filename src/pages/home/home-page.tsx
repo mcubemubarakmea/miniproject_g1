8e5793a85a13bd6c7197b27144a6213b7b754164
Muhsin Mubarak <21nit05@meaec.edu.in>
2024-05-11 01:21:46 +0530

    home page card added

--- src/pages/home/home-page.tsx
+++ src/pages/home/home-page.tsx
@@ -1,18 +1,55 @@
+import { Avatar, Button } from "@mui/material";
 import styles from "./homeStyle.module.css";
 // import { Post } from "./components/post/post";
 // import { AddPost } from "./components/add-post/add-post";
 
 export const Home = () => {
   return (
-    <div className={styles.container}>
-      {/* <AddPost /> */}
-      <div className={styles.postWrap}>
-        {/* <Post /> */}
+    <div className={styles.home}>
+      <div className={styles.box1}>
+        <h3>Name</h3>
+        <h4>Job description</h4>
+        <p>Details</p>
+        <div className={styles.btnwrapper}>
+          <Button
+            sx={{
+              color: "black",
+              backgroundColor: "#48cae4",
+              ":hover": { backgroundColor: "#00b4d8" },
+            }}
+          >
+            Request
+          </Button>
+        </div>
       </div>
-      <div className={styles.postWrap}>
-        {/* <Post /> */}
+      <div className={styles.profile}>
+        <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" />
+        <h3>Name</h3>
       </div>
-      fsfse
+      <div className={styles.homecntr}>
+        <h3>Name</h3>
+        <p>Details</p>
+        <div className={styles.btnwrapper}>
+          <Button
+            sx={{
+              color: "black",
+              backgroundColor: "lightgreen",
+              ":hover": { backgroundColor: "green" },
+            }}
+          >
+            Accept
+          </Button>
+          <Button
+            sx={{
+              color: "black",
+              backgroundColor: "lightred",
+              ":hover": { backgroundColor: "red" },
+            }}
+          >
+            Deny
+          </Button>
+        </div>
+      </div>
     </div>
   );
 };
