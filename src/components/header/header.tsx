719f9e67e6023ed8c2a959cc8b42d589a7d6cd60
Muhsin Mubarak <21nit05@meaec.edu.in>
2024-05-12 15:53:13 +0530

    logout and dropdown

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,13 +1,23 @@
-import { Avatar, Button } from "@mui/material";
+import { Avatar, Menu, MenuItem } from "@mui/material";
 import styles from "./style.module.css";
 import { useAppDispatch, useAppSelector } from "../../store/store";
 import { logout, selectUser, selectUserApiStatus } from "../../store/userSlice";
 import AnoProfileImg from "../../images/ano_profile.png";
+import { MouseEvent, useState } from "react";
 
 export const Header = () => {
   const dispatch = useAppDispatch();
   const user = useAppSelector(selectUser);
   const userApiStatus = useAppSelector(selectUserApiStatus);
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event: MouseEvent<HTMLDivElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    handleLogout();
+    setAnchorEl(null);
+  };
 
   function handleLogout() {
     dispatch(logout());
@@ -18,12 +28,26 @@ export const Header = () => {
       <div className={styles.company}>
         <h1>Labor Link Connect</h1>
       </div>
-      <Button onClick={handleLogout}>logout</Button>
       {userApiStatus === "successfull" && user && (
-        <div className={styles.profile}>
-          <Avatar alt="Remy Sharp" src={AnoProfileImg} />
-          <h3>{user.name}</h3>
-        </div>
+        <>
+          <div className={styles.profile} onClick={(e) => handleClick(e)}>
+            <Avatar alt="Remy Sharp" src={AnoProfileImg} />
+            <h3>{user.name}</h3>
+          </div>
+          <Menu
+            id="basic-menu"
+            anchorEl={anchorEl}
+            open={open}
+            onClose={handleClose}
+            MenuListProps={{
+              "aria-labelledby": "basic-button",
+            }}
+          >
+            <MenuItem sx={{ color: "red" }} onClick={handleClose}>
+              Logout
+            </MenuItem>
+          </Menu>
+        </>
       )}
     </header>
   );
