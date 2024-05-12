3eb41c540ec6f51214afdc7e30a2377935dcab71
Muhsin Mubarak <21nit05@meaec.edu.in>
2024-05-12 12:36:37 +0530

    home page edit

--- src/pages/home/home-page.tsx
+++ src/pages/home/home-page.tsx
@@ -1,4 +1,4 @@
-import { Avatar, Button } from "@mui/material";
+import { Avatar, Button, TextField } from "@mui/material";
 import styles from "./homeStyle.module.css";
 // import { Post } from "./components/post/post";
 // import { AddPost } from "./components/add-post/add-post";
@@ -50,6 +50,32 @@ export const Home = () => {
             Deny
           </Button>
         </div>
+      </div>
+      <div className={styles.contracterchange}>
+        <TextField
+          sx={{ marginLeft: "10px" }}
+          margin="normal"
+          label="Full Name"
+          name="fullname"
+        />
+        <TextField
+          sx={{ marginLeft: "10px" }}
+          margin="normal"
+          label="Phone Number"
+          name="phonenumber"
+        />
+        <TextField
+          sx={{ marginLeft: "10px" }}
+          margin="normal"
+          label="Job Description"
+          name="jobdescription"
+        />
+        <TextField
+          sx={{ marginLeft: "10px" }}
+          margin="normal"
+          label="Details of Workers"
+          name="details"
+        />
       </div>
     </div>
   );
